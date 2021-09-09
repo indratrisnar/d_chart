8ec5a4d511033b3a9fb88a800c57ac26b4e66e21
indratrisnar <trisnaindra1998@gmail.com>
2021-09-10 02:33:11 +0700

    bar chart 1 dimensi beres

--- lib/d_chart.dart
+++ lib/d_chart.dart
@@ -5,20 +5,25 @@ import 'package:flutter/material.dart';
 
 typedef BarColor<Color> = Color Function(
     Map<String, dynamic> barData, int? index);
-Color setColorBar(
-    BarColor<Color> barColor, Map<String, dynamic> barData, int? index) {
-  return barColor(barData, index);
-}
+typedef BarValue<String> = String Function(
+    Map<String, dynamic> barData, int? index);
+
+String defaultBarValue(Map<String, dynamic> barData, int? index) => '';
 
 class DChart {
   static Widget bar({
     required List<Map<String, dynamic>> data,
-    required BarColor<Color> colorBar,
+    required BarColor<Color> barColor,
+    BarValue<String> barValue = defaultBarValue,
+    BarValueAnchor? barValueAnchor,
+    BarValuePosition? barValuePosition,
+    int? barValueFontSize = 14,
+    Color? barValueColor,
+    bool showBarValue = false,
     Color? borderColor,
     double? borderWidth,
     bool? animate,
     Duration? animationDuration,
-    bool interaction = true,
     bool verticalDirection = true,
     bool? showMeasureLine,
     bool? showDomainLine,
@@ -38,39 +43,49 @@ class DChart {
     Color? measureLabelColor,
     int? measureMin,
     int? measureMax,
-    bool showBarValue = false,
-    int? valueInBarFontSize = 14,
-    Color? valueInBarColor,
-    int? valueOutBarFontSize = 14,
-    Color? valueOutBarColor,
+    String? yAxisTitle,
+    Color yAxisTitleColor = Colors.black,
+    int yAxisTitleFontSize = 14,
+    int yAxisTitleInPadding = 8,
+    int yAxisTitleOutPadding = 0,
+    TitlePositionY yAxisTitlePosition = TitlePositionY.left,
+    String? xAxisTitle,
+    Color xAxisTitleColor = Colors.black,
+    int xAxisTitleFontSize = 14,
+    int xAxisTitleInPadding = 8,
+    int xAxisTitleOutPadding = 0,
+    TitlePositionX xAxisTitlePosition = TitlePositionX.bottom,
   }) {
     axisLinePointTick = axisLinePointTick ?? axisLineTick;
     return _charts.BarChart(
       [
         _charts.Series<Map<String, dynamic>, String>(
-          id: 'Sales',
+          id: 'BarChart1',
           domainFn: (Map<String, dynamic> axis, _) => axis['domain'],
           measureFn: (Map<String, dynamic> axis, _) => axis['measure'],
           data: data,
           fillColorFn: (datum, index) {
-            return _charts.ColorUtil.fromDartColor(colorBar(datum, index));
+            return _charts.ColorUtil.fromDartColor(barColor(datum, index));
           },
-          labelAccessorFn: (datum, index) => datum['measure'].toString(),
+          labelAccessorFn: (datum, index) {
+            String? resultValue = barValue(datum, index);
+            return resultValue;
+          },
           strokeWidthPxFn: (datum, index) => borderWidth,
           seriesColor: borderColor == null
               ? null
               : _charts.ColorUtil.fromDartColor(borderColor),
-          overlaySeries: true,
         ),
       ],
       barRendererDecorator: showBarValue
           ? _charts.BarLabelDecorator(
-              labelAnchor: _charts.BarLabelAnchor.end,
+              labelAnchor: _getBarValueAnchor(barValueAnchor),
               outsideLabelStyleSpec:
-                  _textStyleSpec(valueOutBarFontSize, valueOutBarColor),
+                  _textStyleSpec(barValueFontSize, barValueColor),
               insideLabelStyleSpec:
-                  _textStyleSpec(valueInBarFontSize, valueInBarColor),
-              labelPadding: 16,
+                  _textStyleSpec(barValueFontSize, barValueColor),
+              labelPadding: 4,
+              labelPosition: _getBarValuePosition(barValuePosition),
             )
           : null,
       domainAxis: _charts.OrdinalAxisSpec(
@@ -114,7 +129,6 @@ class DChart {
           tickLengthPx: axisLinePointWidth,
           labelRotation: measureLabelRotation,
           labelStyle: _textStyleSpec(measureLabelFontSize, measureLabelColor),
-          // labelJustification: _charts.TickLabelJustification.outside,
           labelOffsetFromAxisPx: measureLabelPaddingToAxisLine,
           labelOffsetFromTickPx: measureLabelPaddingToTick,
         ),
@@ -123,9 +137,26 @@ class DChart {
             ? _charts.NumericExtents(measureMin, measureMax)
             : null,
       ),
+      behaviors: [
+        _charts.ChartTitle(
+          yAxisTitle ?? '',
+          behaviorPosition: _getTitlePositionY(yAxisTitlePosition),
+          titleStyleSpec: _textStyleSpec(yAxisTitleFontSize, yAxisTitleColor),
+          outerPadding: yAxisTitleInPadding,
+          innerPadding: yAxisTitleOutPadding,
+          layoutPreferredSize: yAxisTitle == null ? 0 : null,
+        ),
+        _charts.ChartTitle(
+          xAxisTitle ?? '',
+          behaviorPosition: _getTitlePositionX(xAxisTitlePosition),
+          titleStyleSpec: _textStyleSpec(xAxisTitleFontSize, xAxisTitleColor),
+          outerPadding: xAxisTitleOutPadding,
+          innerPadding: xAxisTitleInPadding,
+          layoutPreferredSize: xAxisTitle == null ? 0 : null,
+        ),
+      ],
       animate: animate,
       animationDuration: animationDuration,
-      defaultInteractions: interaction,
       vertical: verticalDirection,
     );
   }
@@ -136,4 +167,60 @@ class DChart {
       fontSize: fontSize,
     );
   }
+
+  static _charts.BehaviorPosition? _getTitlePositionX(
+      TitlePositionX? position) {
+    switch (position) {
+      case TitlePositionX.top:
+        return _charts.BehaviorPosition.top;
+      case TitlePositionX.bottom:
+        return _charts.BehaviorPosition.bottom;
+      default:
+        return null;
+    }
+  }
+
+  static _charts.BehaviorPosition? _getTitlePositionY(
+      TitlePositionY? position) {
+    switch (position) {
+      case TitlePositionY.left:
+        return _charts.BehaviorPosition.start;
+
+      case TitlePositionY.right:
+        return _charts.BehaviorPosition.end;
+
+      default:
+        return null;
+    }
+  }
+
+  static _charts.BarLabelAnchor? _getBarValueAnchor(BarValueAnchor? position) {
+    switch (position) {
+      case BarValueAnchor.start:
+        return _charts.BarLabelAnchor.start;
+      case BarValueAnchor.middle:
+        return _charts.BarLabelAnchor.middle;
+      case BarValueAnchor.end:
+        return _charts.BarLabelAnchor.end;
+      default:
+        return null;
+    }
+  }
+
+  static _charts.BarLabelPosition _getBarValuePosition(
+      BarValuePosition? position) {
+    switch (position) {
+      case BarValuePosition.inside:
+        return _charts.BarLabelPosition.inside;
+      case BarValuePosition.outside:
+        return _charts.BarLabelPosition.outside;
+      default:
+        return _charts.BarLabelPosition.auto;
+    }
+  }
 }
+
+enum TitlePositionX { top, bottom }
+enum TitlePositionY { left, right }
+enum BarValueAnchor { start, middle, end }
+enum BarValuePosition { inside, outside, auto }

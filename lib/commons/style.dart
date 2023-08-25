002ea770d6db3cfb138154fde688add1ea5a2c6a
indratrisnar <trisnaindra1998@gmail.com>
2023-08-25 16:47:04 +0700

    style for pie

--- lib/commons/style.dart
+++ lib/commons/style.dart
@@ -1,5 +1,21 @@
+// Copyright 2018 the Charts project authors. Please see the AUTHORS file
+// for details.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:community_charts_common/community_charts_common.dart' as common;
 import 'package:flutter/material.dart';
+
 import 'method_common.dart';
 
 class LineStyle {
@@ -47,6 +63,26 @@ class LabelStyle {
     return common.TextStyleSpec(
       color: MethodCommon.chartColor(color),
       fontSize: fontSize,
+    );
+  }
+}
+
+class ArcLabelLeaderLineStyle {
+  final Color color;
+  final double length;
+  final double thickness;
+
+  const ArcLabelLeaderLineStyle({
+    required this.color,
+    required this.length,
+    required this.thickness,
+  });
+
+  common.ArcLabelLeaderLineStyleSpec getRender() {
+    return common.ArcLabelLeaderLineStyleSpec(
+      color: MethodCommon.chartColor(color),
+      length: length,
+      thickness: thickness,
     );
   }
 }
